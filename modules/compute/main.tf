99f5cdbf83513a9d3239dddc3083f861de792a02
Evan Woo <inyongwoo327@gmail.com>
2026-03-11 21:15:17 +0100

    fix(compute): Removed deprecated argument reference

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -5,7 +5,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  region     = data.aws_region.current.name
+  region     = data.aws_region.current.id
   account_id = data.aws_caller_identity.current.account_id
   name_prefix = "project-${local.region}"
 }
